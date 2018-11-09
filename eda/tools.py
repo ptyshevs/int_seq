66ca5e41a0cccd47e532e6fc2571d23d11e3a444
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-11-09 20:47:55 +0200

    Added experiment with polyfit

--- eda/tools.py
+++ eda/tools.py
@@ -1,33 +1,36 @@
 import numpy as np
 import pandas as pd
-
+import warnings
 
 def seq_to_num(sequence, delimiter=',', dtype=np.float64, target_split=True, 
                pad=True, pad_maxlen=100, pad_adaptive=True, 
-               drop_na_inf=True, nbins=1, bins_by='terms'):
+               drop_na_inf=True, nbins=1, bins_by='terms', warn='ignore'):
     """
     Split sequence by delimiter and convert to numbers,
     
     TODO:
-    [X] Pad each bin separatedly
+    [X] Pad each bin adaptively
     [~] Save bin indexes after nan/inf removal (seems to work)
     
     @param delimiter: split each sequence string using this delimiter
     @param dtype: convert each term into number using this data type
     @param pad: add pre-padding if True
+    @param pad_adaptive: pad by median of each bin separatedly if True
     @param pad_maxlen: max length of padded sequence. Cut if longer, pad if shorter.
     @param target_split: split sequences into feature array X and target vector y
     @param drop_na_inf: Filter out np.nan/np.inf values if True (dtype is too small to store a number)
     @param bins_by: sort sequences by # of terms if 'terms', otherwise by max value in sequence
     @param nbins: number of bins. If 1, fallback to no binning.
+    @param warnings: action to make
     
     @return: np.array if pad=False and target_split=False and nbins=1
              tuple (X, y) if target_split=True and nbins=1
              list of np.arrays if nbins > 1 and target_split=False
              list of (X, y, idx) tuples if nbins > 1 and target_split=True
     """
+    warnings.filterwarnings(warn)
     from keras.preprocessing.sequence import pad_sequences
-
+    warnings.resetwarnings()
     num = sequence.str.split(delimiter).map(dtype)
     if nbins > 1:
         split_vals = num.map(lambda x: len(x) if bins_by == 'terms' else max(x)).sort_values()
@@ -62,8 +65,8 @@ def seq_to_num(sequence, delimiter=',', dtype=np.float64, target_split=True,
         if target_split:
             return num[:, :-1], num[:, -1]
         return num
-    if drop_na_inf:
-        num = num[~(np.isnan(num).any(axis=1) | np.isinf(num).any(axis=1))]
+        if drop_na_inf:
+            num = num[~(np.isnan(num).any(axis=1) | np.isinf(num).any(axis=1))]
     if target_split:
         X = num.map(lambda x: x[:-1])
         y = num.map(lambda x: x[-1])
