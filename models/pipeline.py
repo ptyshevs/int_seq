494119512061add968d9325d1cbe6fe8d926ba7c
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-11-11 18:35:48 +0200

    Add stats to Pipeline

--- models/pipeline.py
+++ models/pipeline.py
@@ -11,6 +11,8 @@ class Pipeline:
         @param models: list of (name, model) pairs
         """
         self.models = models
+        self.verbose = verbose
+        self.stat = None
     
     def summary(self):
         maxnamelen = max([len(name) for name, models in self.models])
@@ -18,6 +20,7 @@ class Pipeline:
             print(name.ljust(maxnamelen + 2), model)
     
     def predict(self, data):
+        self.stat = []
         predictions = pd.Series(np.zeros(len(data)), index=data.index)
         indices_solved = []
         for name, model in self.models[:-1]:
@@ -26,10 +29,21 @@ class Pipeline:
                 print(f"solved by {name}: {len(ind)}")
             predictions[ind] = pred
             indices_solved += ind
+            self.stat.append((name, len(pred)))
         unsolved_seq = data[~data.index.isin(indices_solved)] 
-        fallback_model = self.models[-1][1]
+        fallback_name, fallback_model = self.models[-1]
         if self.verbose:
-            print(f"solved by fallback-model ({fallback_model}): {len(unsolved_seq)}")
+            print(f"solved by fallback-model {fallback_name}: {len(unsolved_seq)}")
+        self.stat.append((fallback_name, len(unsolved_seq)))
         predictions[~predictions.index.isin(indices_solved)] = fallback_model.predict(unsolved_seq)
         return predictions
-        
+    
+    @property
+    def stat_(self):
+        """
+        Display statistics after prediction was made
+        """
+        if self.stat is None:
+            raise ValueError("Use .predict method to collect statistics")
+        for name, nsolved in self.stat:
+            print(f"solved by {name.ljust(6)}: {str(nsolved).rjust(8)}")
