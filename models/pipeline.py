abfd4e2a24bc41ce882a67ed076f69e47e084868
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-11-13 00:01:13 +0200

    Finished experiment with LSTM, move fallback model outside in Pipeline

--- models/pipeline.py
+++ models/pipeline.py
@@ -2,15 +2,17 @@ import numpy as np
 import pandas as pd
 
 class Pipeline:
-    def __init__(self, models, verbose=False):
+    def __init__(self, models, fallback=None, verbose=False):
         """
         Pipeline is a collection of algorithms used sequentially to predict target.
         Every algorithm (except for fallback) should return tuple of (sequences_solved, indices, prediction)
         
-        Last model in a list is considered fallback-prediction.
         @param models: list of (name, model) pairs
+        @param fallback: model used on sequences that remained unsolved after passing through all models.
+        @param verbose: output additional information during predict
         """
         self.models = models
+        self.fallback = fallback
         self.verbose = verbose
         self.stat = None
     
@@ -18,24 +20,37 @@ class Pipeline:
         maxnamelen = max([len(name) for name, models in self.models])
         for name, model in self.models:
             print(name.ljust(maxnamelen + 2), model)
+        if self.fallback:
+            print(f"Fallback model: {self.fallback}")
+        else:
+            print("No fallback model")
     
     def predict(self, data):
+        """
+        Pass data through the pipeline
+        
+        @return:
+            If fallback model was specified, series of predictions returned for whole
+            data. Otherwise, return indices of sequences solved and corresponding predictions.
+        """
         self.stat = []
         predictions = pd.Series(np.zeros(len(data)), index=data.index)
         indices_solved = []
-        for name, model in self.models[:-1]:
+        for name, model in self.models:
             _, ind, pred = model.predict(data[~data.index.isin(indices_solved)])
             if self.verbose:
                 print(f"solved by {name}: {len(ind)}")
             predictions[ind] = pred
             indices_solved += ind
             self.stat.append((name, len(pred)))
+        if self.fallback is None:  # no fallback model - return indices and predicitons
+            predictions = predictions[predictions.index.isin(indices_solved)]
+            return indices_solved, predictions
         unsolved_seq = data[~data.index.isin(indices_solved)] 
-        fallback_name, fallback_model = self.models[-1]
         if self.verbose:
-            print(f"solved by fallback-model {fallback_name}: {len(unsolved_seq)}")
-        self.stat.append((fallback_name, len(unsolved_seq)))
-        predictions[~predictions.index.isin(indices_solved)] = fallback_model.predict(unsolved_seq)
+            print(f"solved by fallback-model {self.fallback}: {len(unsolved_seq)}")
+        self.stat.append((str(self.fallback), len(unsolved_seq)))
+        predictions[unsolved_seq.index] = self.fallback.predict(unsolved_seq)
         return predictions
     
     @property
