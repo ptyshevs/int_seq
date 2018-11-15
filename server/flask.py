add0977bfdc49fa116099d1c9068769321a63f17
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-11-15 23:47:24 +0200

    Server playfield

--- server/flask.py
+++ server/flask.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request, flash
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def entry_point():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def form_process():
+    text = request.form['text']
+    # print(text)
+    print(request.values)
+    return render_template('index.html', form=form_process, text=text)
+
+
+
+@app.route('/ping', methods=['GET'])
+def another():
+    return jsonify('hi')
+
+@app.route('/page')
+def static_page():
+    return render_template('index.html')
