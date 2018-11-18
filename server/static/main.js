a671fdbffc4aa0815fc3e7d30f6b1536666e6fbb
AlexStudent <sanchofeed@gmail.com>
2018-11-18 22:50:43 +0200

    Modified front-end part of our application

---
+++ server/static/main.js
@@ -0,0 +1,21 @@
+function postload() {
+    setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("preloader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+function getNav() {
+    var nav = document.getElementById('navbar');
+    var btn = document.getElementById('sandwich');
+    if (nav.className === 'topnav') {
+        nav.className += ' responsive';
+        btn.innerHTML = '&#10799;';
+    }
+    else {
+        nav.className = 'topnav';
+        btn.innerHTML = '&#9776;';
+    }
+}
